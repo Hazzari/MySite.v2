db73be72029bc4d91df62217f5e7486d97a4e247
Hazzari <dev.aleksan@gmail.com>
2020-09-25 13:50:17 +0300

    style(project): Удалил лишние фото из шаблонов, разделил шаблон home_page на модули

--- blog/templatetags/sidebar.py
+++ blog/templatetags/sidebar.py
@@ -1,14 +1,24 @@
 from django import template
 
-from blog.models import Post
+from blog.models import Post, Tag
 
 register = template.Library()
 
 
 @register.inclusion_tag(filename='blog/popular_post_tpl.html')
-def get_popular(name_post, cnt=3):
+def get_popular(name_post: str, cnt=3) -> dict:
     """
     показываем популярные статьи за исключением открытой
     """
+    print(f'name_post = {name_post}')
     posts = Post.objects.order_by('-views').exclude(slug__exact=name_post)[:cnt]
     return {'posts': posts}
+
+
+@register.inclusion_tag(filename='blog/tags_tpl.html')
+def get_tags() -> dict:
+    """
+    показываем теги
+    """
+    tags = Tag.objects.all()
+    return {'tags': tags}
